a92be099d205f513fb55ddd6fc32e29cbb933779
Ghazi <gnehdi@amayas.fr>
2020-12-30 20:06:20 +0100

    add fonts + images

--- src/app/material-ui/material-ui.module.ts
+++ src/app/material-ui/material-ui.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
-import { MatButtonModule } from '@angular/material';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 
 @NgModule({
+  imports: [
+    MatButtonModule,
+    MatToolbarModule
+  ],
   exports: [
     MatButtonModule,
     MatToolbarModule
