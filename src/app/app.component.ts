02a1e4c0d76d23b74ff54b61df71c28be6c4ba7e
ghaziNahdi94 <ghazinehdi@gmail.com>
2021-01-02 21:24:57 +0100

    add education

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { Title } from '@angular/platform-browser';
-import { Experience } from './models';
 
 @Component({
   selector: 'app-root',
@@ -8,12 +7,9 @@ import { Experience } from './models';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
-
   constructor(private titleService: Title) {}
 
   public ngOnInit(): void {
     this.titleService.setTitle('Ghazi NEHDI');
   }
-
- 
 }
